83774f3befd05521b8d53bc8d19cf71f9a3be91d
kotchamon <kotchamon.momay@gmail.com>
2020-03-04 12:21:22 +0700

    DAO LAB10

---
+++ src/Employee.java
@@ -0,0 +1,59 @@
+public class Employee {
+    //attributes
+    private int empID;
+    private String name;
+    private String position;
+    private Double salary;
+
+    //to string
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empID=" + empID +
+                ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", salay=" + salary +
+                '}';
+    }
+
+    //getter and setter
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalay(Double salary) {
+        this.salary = salary;
+    }
+
+    //constructor
+    public Employee(int empID, String name, String position, Double salary) {
+        this.empID = empID;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+}
